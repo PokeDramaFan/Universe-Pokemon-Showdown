eb95e3d8f661b43a0cc8803434fc431e316559be
PokeDramaFan <smilerjosue@hotmail.com>
2015-09-06 11:24:58 -0600

    Update config-example.js

--- config/config-example.js
+++ config/config-example.js
@@ -12,7 +12,7 @@ exports.proxyip = ['127.0.0.0/8'];
 
 exports.serverid = 'universe';
 
-exports.servertoken = 'Cfgm58SedzmZ;
+exports.servertoken = 'Cfgm58SedzmZ';
 
 // Pokemon of the Day - put a pokemon's name here to make it Pokemon of the Day
 //   The PotD will always be in the #2 slot (not #1 so it won't be a lead)
